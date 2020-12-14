9dd88e9be7be8aff5b1f8b0519dfa0d3640deb13
Bob Boi <akgaming1322@gmail.com>
2020-12-14 14:27:15 -0500

    Updates

---
+++ Assets/Scripts/FucckUpTheGravity.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FucckUpTheGravity : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject Player;
+    public Camera Boi;
+    public GameObject Targ;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Targ.transform.position = new Vector2(Player.transform.position.x, transform.position.y);
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+        //Boi.GetComponent<CameraControl>().target = Targ;
+        Player.GetComponent<Rigidbody2D>().gravityScale = -0.01f;
+    }
+}
--- Assets/Scripts/TriggerEnding.cs
+++ Assets/Scripts/TriggerEnding.cs
@@ -6,6 +6,7 @@ using UnityEngine.SceneManagement;
 public class TriggerEnding : MonoBehaviour
 {
     // Start is called before the first frame update
+    public GameObject Player;
     void Start()
     {
             
@@ -19,6 +20,8 @@ public class TriggerEnding : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        //DontDestroyOnLoad(Player);
         SceneManager.LoadScene(2, LoadSceneMode.Single);
+
     }
 }
